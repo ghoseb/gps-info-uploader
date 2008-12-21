3efbab7a4d030e4e89abed444446c01d22cf9c5e
Baishampayan Ghose <b.ghose@gmail.com>
2008-12-21 16:15:24 +0530

    Initial (non-working) version of the Python code

---
+++ gps-info-uploader.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+## gps-info-uploader.py -- Upload GPS Info -*- Python -*-
+## Time-stamp: "2008-12-21 16:13:14 ghoseb"
+
+## Copyright (c) 2008, Baishampayan Ghose <b.ghose at gmail.com>
+"""
+Regularly gather information from the mobile phone's GPS receiver and
+upload the information to the server.
+"""
+
+import httplib
+import positioning
+
+SERVER_URL = "server.url"
+SERVER_PATH = "/some/path"
+HTTP_METHOD = "POST"
+
+# Write more stuff here.
+
+
+
