773b5204db2b2843c7dc507d4b0612d1c34f2327
Baishampayan Ghose <b.ghose@ocricket.com>
2008-12-23 19:16:28 +0530

    Added a primitive function to log the info in a file for now.

--- gps-info-uploader.py
+++ gps-info-uploader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 ## gps-info-uploader.py -- Upload GPS Info -*- Python -*-
-## Time-stamp: "2008-12-21 16:13:14 ghoseb"
+## Time-stamp: "2008-12-23 19:15:32 ghoseb"
 
 ## Copyright (c) 2008, Baishampayan Ghose <b.ghose at gmail.com>
 """
@@ -8,14 +8,35 @@ Regularly gather information from the mobile phone's GPS receiver and
 upload the information to the server.
 """
 
-import httplib
+# import httplib
 import positioning
 
-SERVER_URL = "server.url"
-SERVER_PATH = "/some/path"
-HTTP_METHOD = "POST"
+# SERVER_URL = "server.url"
+# SERVER_PATH = "/some/path"
+# HTTP_METHOD = "POST"
 
-# Write more stuff here.
+LOG_FILE = "E:\\gps_log.txt"
+
+positioning.select_module(positioning.default_module())
+positioning.set_requestors([{"type":"service",
+                             "format":"application",
+                             "data":"gps_info_getter"}])
+
+positioning.last_position()
+
+def callback_fn(event):
+    """Callback to log GPS info
+    
+    Arguments:
+    - `event`: The info from a GPS event
+    """
+    fp = open(LOG_FILE, "a")
+    fp.write(event)
+    fp.close()
+
+print "I think GPS is working, so trying to log the info..."
+
+positioning.position(callback=callback_fn, partial=0, interval=500000)
 
 
 
